76405a07e12498a8b59d31d41250fe1baa3233bc
HerbertNtim <herbertntim15@gmail.com>
2025-06-03 23:41:12 +0000

    dev: navbar welcome

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import { useState } from "react";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
+const navItems = ["Market", "Exchange", "Tutorial", "Wallets"];
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-export default Navbar
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <img
+          src="../../images/logo.png"
+          alt="logo"
+          className="w-32 cursor-pointer"
+        />
+      </div>
+
+      <ul className="text-white md:flex hidden items-center justify-between">
+        {navItems.map((item, index) => (
+          <li key={index} className="mx-4 font-bold leading-2">
+            {item}
+          </li>
+        ))}
+        <li className="bg-[#2952e3] px-4 py-2 rounded-full ml-6 font-medium hover:bg-[#2546bd] cursor-pointer">
+          Login
+        </li>
+      </ul>
+
+      <div className="flex relative">
+        {!toggleMenu && (
+          <HiMenuAlt4
+            fontSize={28}
+            className="text-white cursor-pointer md:hidden"
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="z-10 fixed top-0 -right-2 md:hidden blue-glassmorphism animate-slide-in w-[70vw] h-screen shadow-xl">
+            <button className="text-xl w-full my-2 p-4">
+              <AiOutlineClose
+                fontSize={28}
+                className="text-white cursor-pointer md:hidden"
+                onClick={() => setToggleMenu(false)}
+              />
+            </button>
+            <ul className="flex flex-col gap-4 text-white items-end justify-center">
+              {navItems.map((item, index) => (
+                <li key={index} className="mx-4 my-2">
+                  {item}
+                </li>
+              ))}
+              <li className="bg-[#2952e3] mr-2 px-4 py-2 rounded-md ml-6 font-medium hover:bg-[#2546bd] cursor-pointer">
+                Login
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+const commonStyle =
+  "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 const Welcome = () => {
   return (
-    <div>Welcome</div>
-  )
-}
+    <div className="flex w-full justify-center items-center">
+      <div className="flex md:flex-row flex-col justify-between items-start md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col md:mr-10">
+          <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+            Send Crypto <br /> across the world
+          </h1>
+          <p className="font-light text-white text-base md:w-9/12 w-11/12 text-left mt-5">
+            Explore the crypto world. Buy and Sell cryptocurrencies easily on
+            Krypto.
+          </p>
 
-export default Welcome
+          <button className="flex flex-row justify-center items-center bg-[#2952e3] cursor-pointer hover:bg-[#2546bd] text-white my-5 py-2 px-4 rounded-md">
+            Connect Wallet
+          </button>
+
+          <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+            <div className={`${commonStyle} rounded-tl-2xl`}>Reliability</div>
+            <div className={`${commonStyle}`}>Security</div>
+            <div className={`${commonStyle} rounded-tr-2xl`}>Ethereum</div>
+            <div className={`${commonStyle} rounded-bl-2xl`}>Web 3.0</div>
+            <div className={`${commonStyle}`}>Low Fees</div>
+            <div className={`${commonStyle} rounded-br-2xl`}>Blockchain</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
